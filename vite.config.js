be33d3df8118c87fd5dfde10c616d83170dbca76
Abdullah Khan <msg.abdullah@gmail.com>
2026-05-07 23:23:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // ONLY strictly necessary if using GitHub Pages. 
+  // Vercel ignores this and routes perfectly without it.
+  base: '/trading-app/', 
 })
